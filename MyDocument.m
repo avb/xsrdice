c72acfaab85ed99ae07705a07944e86abf0fd406
Adam Brown <avb@dicebag.com>
2009-04-17 15:28:37 -0500

    Initial check-in

---
+++ MyDocument.m
@@ -0,0 +1,92 @@
+//
+//  MyDocument.m
+//  XSRDice
+//
+//  Created by Adam Brown on 11/20/07.
+//
+//
+//  Copyright © 2008 Adam Brown (Dicebag Games). All rights reserved.
+//  
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are
+//  met:
+//  
+//  1. Redistributions of source code must retain the above copyright
+//  notice, this list of conditions and the following disclaimer.
+//  
+//  2. Redistributions in binary form must reproduce the above copyright
+//  notice, this list of conditions and the following disclaimer in the
+//  documentation and/or other materials provided with the distribution.
+//  
+//  THIS SOFTWARE IS PROVIDED BY DICEBAG GAMES ``AS IS'' AND ANY EXPRESS OR
+//  IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+//  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//  DISCLAIMED. IN NO EVENT SHALL THE SHIIRA PROJECT OR CONTRIBUTORS BE
+//  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+//  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+//  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+//  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+//  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+//  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+//  THE POSSIBILITY OF SUCH DAMAGE.
+//  
+//
+
+#import "MyDocument.h"
+
+@implementation MyDocument
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+		
+        // Add your subclass-specific initialization here.
+        // If an error occurs here, send a [self release] message and return nil.
+		
+    }
+    return self;
+}
+
+- (NSString *)windowNibName
+{
+    // Override returning the nib file name of the document
+    // If you need to use a subclass of NSWindowController or if your document supports multiple NSWindowControllers, you should remove this method and override -makeWindowControllers instead.
+    return @"MyDocument";
+}
+
+- (void)windowControllerDidLoadNib:(NSWindowController *) aController
+{
+    [super windowControllerDidLoadNib:aController];
+    // Add any code here that needs to be executed once the windowController has loaded the document's window.
+}
+
+- (NSData *)dataOfType:(NSString *)typeName error:(NSError **)outError
+{
+    // Insert code here to write your document to data of the specified type. If the given outError != NULL, ensure that you set *outError when returning nil.
+	
+    // You can also choose to override -fileWrapperOfType:error:, -writeToURL:ofType:error:, or -writeToURL:ofType:forSaveOperation:originalContentsURL:error: instead.
+	
+    // For applications targeted for Panther or earlier systems, you should use the deprecated API -dataRepresentationOfType:. In this case you can also choose to override -fileWrapperRepresentationOfType: or -writeToFile:ofType: instead.
+	
+    if ( outError != NULL ) {
+		*outError = [NSError errorWithDomain:NSOSStatusErrorDomain code:unimpErr userInfo:NULL];
+	}
+	return nil;
+}
+
+- (BOOL)readFromData:(NSData *)data ofType:(NSString *)typeName error:(NSError **)outError
+{
+    // Insert code here to read your document from the given data of the specified type.  If the given outError != NULL, ensure that you set *outError when returning NO.
+	
+    // You can also choose to override -readFromFileWrapper:ofType:error: or -readFromURL:ofType:error: instead. 
+    
+    // For applications targeted for Panther or earlier systems, you should use the deprecated API -loadDataRepresentation:ofType. In this case you can also choose to override -readFromFile:ofType: or -loadFileWrapperRepresentation:ofType: instead.
+    
+    if ( outError != NULL ) {
+		*outError = [NSError errorWithDomain:NSOSStatusErrorDomain code:unimpErr userInfo:NULL];
+	}
+    return YES;
+}
+
+@end
---
+++ XSRDice.m
@@ -0,0 +1,199 @@
+//
+//  XSRDice.m
+//  XSRDice
+//
+//  Created by Adam Brown on 11/20/07.
+//
+//
+//  Copyright © 2008 Adam Brown (Dicebag Games). All rights reserved.
+//  
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are
+//  met:
+//  
+//  1. Redistributions of source code must retain the above copyright
+//  notice, this list of conditions and the following disclaimer.
+//  
+//  2. Redistributions in binary form must reproduce the above copyright
+//  notice, this list of conditions and the following disclaimer in the
+//  documentation and/or other materials provided with the distribution.
+//  
+//  THIS SOFTWARE IS PROVIDED BY DICEBAG GAMES ``AS IS'' AND ANY EXPRESS OR
+//  IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+//  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//  DISCLAIMED. IN NO EVENT SHALL THE SHIIRA PROJECT OR CONTRIBUTORS BE
+//  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+//  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+//  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+//  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+//  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+//  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+//  THE POSSIBILITY OF SUCH DAMAGE.
+//  
+//
+
+#import "XSRDice.h"
+
+
+@implementation XSRDice
+
+- (IBAction)rollBasic:(id)sender
+{
+	NSInteger dicePool;
+	NSInteger roll;
+	NSString *rollToPrint;
+	NSMutableArray *diceRolls;
+	NSString *rollList;
+	NSString *glitch;
+	NSInteger i = 0;
+	NSInteger hits = 0;
+	NSInteger glitchcounter = 0;
+	NSInteger glitchcheck = 0;	
+	
+	dicePool = [basicDicePool intValue];
+	diceRolls = [[NSMutableArray alloc] init];
+	rollList = [[NSString alloc] initWithString:@"Dice Rolls: "];
+
+
+	if (dicePool > 0) 
+	{
+		
+		for (i = 0; i < dicePool; i++)
+		{
+			
+			roll = (random() % 6) + 1;
+			
+			if (roll >= 5) {
+				hits++;
+				if (roll == 6 && basicRuleOfSix == 1) {
+					dicePool++;
+				}
+			}
+			
+			if (roll == 1) {
+				glitchcounter++;
+			}
+			
+			[diceRolls insertObject:[NSNumber numberWithInt:roll] atIndex:i];
+		}
+
+		[diceRolls sortUsingSelector:@selector(compare:)];
+		
+		glitchcheck = ((dicePool/2) + (dicePool%2));
+		
+		if (glitchcounter >= glitchcheck) {
+			if (hits > 0){
+				glitch = [[NSString alloc] initWithString:@"Glitch\n"];
+			} else if (hits == 0){
+				glitch = [[NSString alloc] initWithString:@"Critical Glitch\n"];
+			}
+		} else {
+			glitch = [[NSString alloc] initWithString:@" "];
+		}
+		
+		for (i = dicePool-1; i >= 0; i--) {
+			rollToPrint = [diceRolls objectAtIndex:i];
+			rollList = [rollList stringByAppendingFormat:@"%@ ", rollToPrint];
+		}
+		
+		NSString *outputString = [[NSString alloc] initWithFormat:@"Total Dice Pool: %d \n%@\nNumber of Hits: %d\n%@\n", dicePool, rollList, hits, glitch];
+		
+		
+		[[basicTextView textStorage] replaceCharactersInRange:NSMakeRange(0, 0)
+												   withString:outputString];
+
+	} else {
+		NSBeep();
+	}
+}
+
+- (IBAction)rollAdvanced:(id)sender
+{
+	NSInteger dicePool;
+	NSInteger roll;
+	NSString *rollToPrint;
+	NSMutableArray *diceRolls;
+	NSString *rollList;
+	NSString *glitch;
+	NSString *defaultTest;
+	NSInteger i = 0;
+	NSInteger hits = 0;
+	NSInteger glitchcounter = 0;
+	NSInteger glitchcheck = 0;	
+	
+	if (![advancedSkillPool intValue] && [advancedAttributePool intValue] >= 1) 
+	{
+		dicePool = [advancedAttributePool intValue] + [advancedEdgePool intValue] + [advancedModifierPool intValue] - 1;
+		defaultTest = [[NSString alloc] initWithString:@"Default Test\n"];
+	} else 
+	{
+		dicePool = [advancedAttributePool intValue] + [advancedSkillPool intValue] + [advancedEdgePool intValue] + [advancedModifierPool intValue];
+		if (dicePool == [advancedEdgePool intValue]) {
+			defaultTest = [[NSString alloc] initWithString:@"Longshot Test\n"];
+		} else {
+		defaultTest = [[NSString alloc] initWithString:@""];
+	}
+	}
+	diceRolls = [[NSMutableArray alloc] init];
+	rollList = [[NSString alloc] initWithString:@"Dice Rolls: "];
+	
+	
+	if (dicePool > 0) {
+		
+		for (i = 0; i < dicePool; i++)
+		{
+			
+			roll = (random() % 6) + 1;
+			
+			if (roll >= 5) {
+				hits++;
+				if (roll == 6 && [advancedEdgePool intValue]) {
+					dicePool++;
+				}
+			}
+			
+			if (roll == 1) {
+				glitchcounter++;
+			}
+			
+			[diceRolls insertObject:[NSNumber numberWithInt:roll] atIndex:i];
+		}
+		
+		[diceRolls sortUsingSelector:@selector(compare:)];
+		
+		glitchcheck = ((dicePool/2) + (dicePool%2));
+		
+		if (glitchcounter >= glitchcheck) {
+			if (hits > 0){
+				glitch = [[NSString alloc] initWithString:@"Glitch\n"];
+			} else if (hits == 0){
+				glitch = [[NSString alloc] initWithString:@"Critical Glitch\n"];
+			}
+		} else {
+			glitch = [[NSString alloc] initWithString:@" "];
+		}
+		
+		for (i = dicePool-1; i >= 0; i--) {
+			rollToPrint = [diceRolls objectAtIndex:i];
+			rollList = [rollList stringByAppendingFormat:@"%@ ", rollToPrint];
+		}
+		
+		NSString *outputString = [[NSString alloc] initWithFormat:@"%@Total Dice Pool: %d \n%@\nNumber of Hits: %d\n%@\n", defaultTest, dicePool, rollList, hits, glitch];
+		
+		[[advancedTextView textStorage] replaceCharactersInRange:NSMakeRange(0, 0)
+												   withString:outputString];
+		
+	} else {
+		NSBeep();
+	}
+	
+}
+	
+
+- (IBAction)basicRuleOfSix:(id)sender
+{
+	basicRuleOfSix = 1;
+}
+
+
+@end
